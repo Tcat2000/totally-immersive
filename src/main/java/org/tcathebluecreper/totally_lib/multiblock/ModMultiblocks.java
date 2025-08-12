21921c2f97ce72a0fb4d2701acc3b6c2e71059c8
Taylor <taylordspivey@gmail.com>
2025-08-12 12:24:11 -0400

    Revert "Work on events"
    This reverts commit 834ab5e80d714636ed4dbe817424f27c89cb354a.

--- src/main/java/org/tcathebluecreper/totally_lib/multiblock/ModMultiblocks.java
+++ src/main/java/org/tcathebluecreper/totally_lib/multiblock/ModMultiblocks.java
@@ -2,14 +2,13 @@ package org.tcathebluecreper.totally_lib.multiblock;
 
 import net.minecraftforge.common.MinecraftForge;
 import org.tcathebluecreper.totally_lib.TotallyLibrary;
-import org.tcathebluecreper.totally_lib.kubejs.Plugin;
-import org.tcathebluecreper.totally_lib.kubejs.TLMultiblockRegistrationEventJS;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class ModMultiblocks {
-    public static List<RegisterableMultiblock> allMultiblocks = new ArrayList<>();
+    private static List<RegisterableMultiblock> allMultiblocks = new ArrayList<>();
     public static void init() {
+        MinecraftForge.EVENT_BUS.post(new TLMultiblockRegistrationEvent(TotallyLibrary.regManager, allMultiblocks::add));
     }
 }
